6333389e04a220ad67f3d0e71f6cd62aed3b3598
Amanda Lourenco <amandalourenco67@gmail.com>
2020-05-05 19:50:11 -0300

    primeiro commit

---
+++ amanda/src/main/java/com/amanda/AmandaApplication.java
@@ -0,0 +1,13 @@
+package com.amanda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmandaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmandaApplication.class, args);
+	}
+
+}
